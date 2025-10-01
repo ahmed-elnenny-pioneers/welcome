bc869b29e44df033029fb484aecb6bc96946e042
Ahmed <ahmed.elnenny@pioneers-solutions.com>
2025-10-01 16:58:50 +0300

    ..

--- Program.cs
+++ Program.cs
@@ -4,8 +4,6 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World #1"); ///bug1 fix
         Console.WriteLine("Hello, World #1");
-        Console.WriteLine("Hello, World F #1");
     }
 }
